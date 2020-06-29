64945f2526c52ac3607f378d9f26a0453bc53ad5
ZHJ0125 <shandonghoujin@163.com>
2020-06-29 16:30:16 +0800

    Do Linux Training

--- linux-2.6.24/arch/arm/mach-s3c2410/mach-smdk2410.c
+++ linux-2.6.24/arch/arm/mach-s3c2410/mach-smdk2410.c
@@ -54,6 +54,66 @@
 
 #include <asm/plat-s3c24xx/common-smdk.h>
 
+/* ZhangHoujin Edited in 2020.06.29 */
+#include <asm/arch /fb.h>
+#include <linux/platform_device.h>
+static struct s3c2410fb_display up2410_fb[] __initdata = 
+{
+	{
+		.lcdcon5 = (1<<12)|(1<<11)|(1<<9)|(1<<8)|(1<<0),
+		.type = (3<<5),
+		.width = 640,
+		.height = 480,
+		.pixclock = 39721,
+		.xres = 640,
+		.yres = 480,
+		.bpp = 16,
+		.left_margin = 40,
+		.right_margin = 32,
+		.hsync_len = 32,
+		.vsync_len = 2,
+		.upper_margin = 35,
+		.lower_margin = 5,
+	},
+};
+
+static struct s3c2410fb_mach_info up2410_fb_info __initdata = 
+{
+	.displays = up2410_fb,
+	.num_displays = 1,
+	.default_display = 0,
+	.gpcup = 0xffffffff,
+	.gpcup_mask = 0x0,
+	.gpccon = 0xaaaaaaaa,
+	.gpccon_mask = 0x0,
+	.gpdup = 0xffffffff,
+	.gpdup_mask = 0x0,
+	.gpdcon = 0xaaaaaaaa,
+	.gpdcon_mask = 0x0,
+	.lpcsel = 0,
+};
+static struct resource s3c_dm9ks_resource[] = {
+	[0] = {
+		.start = 0x10000000,
+		.end = 0x10000040,
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = IRQ_EINT2,
+		.end = IRQ_EINT2,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+struct platform_device s3c_device_dm9ks = {
+	.name = "s3c2410-dm9ks",
+	.id = -1,
+	.num_resources = ARRAY_SIZE(s3c_dm9ks_resource),
+	.resource = s3c_dm9ks_resource,
+};
+
+/* End of Edited */
+
 static struct map_desc smdk2410_iodesc[] __initdata = {
   /* nothing here yet */
 };
@@ -92,6 +152,7 @@ static struct platform_device *smdk2410_devices[] __initdata = {
 	&s3c_device_wdt,
 	&s3c_device_i2c,
 	&s3c_device_iis,
+	&s3c_device_dm9ks,
 };
 
 static void __init smdk2410_map_io(void)
@@ -105,6 +166,7 @@ static void __init smdk2410_init(void)
 {
 	platform_add_devices(smdk2410_devices, ARRAY_SIZE(smdk2410_devices));
 	smdk_machine_init();
+	s3c24xx_fb_set_platdata(&up2410_fb_info);
 }
 
 MACHINE_START(SMDK2410, "SMDK2410") /* @TODO: request a new identifier and switch
